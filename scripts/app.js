5c7f71628bb1d7325ab7302903ba5d754c97d98f
Pasha Kirillov <89199389177lol@gmail.com>
2023-11-19 19:33:20 +0300

    добавил проценты, переход между тестами, итог

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const headElem = document.getElementById("head");
 const buttonsElem = document.getElementById("buttons");
 const pagesElem = document.getElementById("pages");
+var percents = 0;
 
 //Класс, который представляет сам тест
 class Quiz
@@ -41,7 +42,7 @@ class Answer
 }
 
 var questions = [];
-
+////////////////////////// ДЕРЕВО 3.2 ////////////////////////////////////////////
 questions[0] = {
 	q: new Question("Состояние зубных рядов", 0, true, false,
 	[
@@ -65,7 +66,7 @@ questions[1] = {
 questions[2] = {
 	q: new Question("Перекрестный прикус (нижние премоляры лингвально)", 2, false, false,
 	[
-		new Answer("Сопоставить в конструктивынй прикус и определить степень сужения- возможно требуется протракция нижней челюсти в комбинации с зубоальвеолярным расширением нижнего зр", 0)
+		new Answer("Сопоставить в конструктивынй прикус и определить степень сужения- возможно требуется протракция нижней челюсти в комбинации с зубоальвеолярным расширением нижнего зр", 11)
 	])
 }
 questions[3] = {
@@ -78,7 +79,8 @@ questions[3] = {
 questions[4] = {
 	q: new Question("Результат", 4, false, true,
 	[
-		new Answer("Сужение нижнего зубного ряда, верхний зр в норме", -1)
+		new Answer("Сужение нижнего зубного ряда, верхний зр в норме", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[5] = {
@@ -91,7 +93,7 @@ questions[5] = {
 questions[6] = {
 	q: new Question("Сверхкомплектные зубы", 6, false, false,
 	[
-		new Answer("Консультация с хирургом о времени удаления, перспективы прорезывания постоянных зубов", -1)
+		new Answer("Консультация с хирургом о времени удаления, перспективы прорезывания постоянных зубов", 16)
 	])
 }
 questions[7] = {
@@ -124,7 +126,8 @@ questions[10] = {
 questions[11] = {
 	q: new Question("Результат", 11, false, true,
 	[
-		new Answer("Сопоставить в конструктивынй прикус и определить степень сужения- возможно требуется протракция нижней челюсти в комбинации с зубоальвеолярным расширением нижнего зр", -1)
+		new Answer("Сопоставить в конструктивынй прикус и определить степень сужения- возможно требуется протракция нижней челюсти в комбинации с зубоальвеолярным расширением нижнего зр", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[12] = {
@@ -145,53 +148,61 @@ questions[13] = {
 questions[14] = {
 	q: new Question("Результат", 14, false, true,
 	[
-		new Answer("Перспективы и второй этап лечения", -1)
+		new Answer("Перспективы и второй этап лечения", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[15] = {
 	q: new Question("Результат", 15, false, true,
 	[
-		new Answer("Компьютерная томография - оценка расположения клыков и премоляров", 24)
+		new Answer("Компьютерная томография - оценка расположения клыков и премоляров", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[16] = {
 	q: new Question("Результат", 16, false, true,
 	[
-		new Answer("Консультация с хирургом о времени удаления, перспективы прорезывания постоянных зубов", -1)
+		new Answer("Консультация с хирургом о времени удаления, перспективы прорезывания постоянных зубов", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[17] = {
 	q: new Question("Результат", 17, false, true,
 	[
-		new Answer("Ротация моляров: аппарат для деротации моляров", -1)
+		new Answer("Ротация моляров: аппарат для деротации моляров", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[18] = {
 	q: new Question("Результат", 18, false, true,
 	[
-		new Answer("Мезиальная ангуляция: элемент для изменения ангуляции постоянного моляра", -1)
+		new Answer("Мезиальная ангуляция: элемент для изменения ангуляции постоянного моляра", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[19] = {
 	q: new Question("Результат", 19, false, true,
 	[
-		new Answer("Сохраняется место зр, отсутствуют нарушения в положении первого моляра", -1)
+		new Answer("Сохраняется место зр, отсутствуют нарушения в положении первого моляра", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[20] = {
 	q: new Question("Результат", 20, false, true,
 	[
-		new Answer("Ротация моляров: аппарат для деротации моляров", -1)
+		new Answer("Ротация моляров: аппарат для деротации моляров", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[21] = {
 	q: new Question("Смещение зубов в сторону дефекта", 21, false, false,
 	[
-		new Answer("Рукообразный захват съемным аппарато, частичная брекет-система", 29)
+		new Answer("Рукообразный захват съемным аппарато, частичная брекет-система", 29),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[22] = {
-	q: new Question("Результат", 22, false, false,
+	q: new Question("Уплощение формы зубного ряда", 22, false, false,
 	[
 		new Answer("Губной бампер, частичная брекет-система", 30)
 	])
@@ -199,51 +210,59 @@ questions[22] = {
 questions[23] = {
 	q: new Question("Результат", 23, false, true,
 	[
-		new Answer("Небный расширитель/одночелюстной аппарат с винтом", -1)
+		new Answer("Небный расширитель/одночелюстной аппарат с винтом", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[24] = {
-	q: new Question("Cужение верхнего и нижнего зубного ряда", 24, false, true,
+	q: new Question("Результат", 24, false, true,
 	[
-		new Answer("небный расширтель на верхнюю челюсть, работа с остеопатом, оценка потенциала роста по данным ТРГ в боковой проекции", -1)
+		new Answer("Небный расширтель на верхнюю челюсть, работа с остеопатом, оценка потенциала роста по данным ТРГ в боковой проекции", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[25] = {
 	q: new Question("Результат", 25, false, true,
 	[
-		new Answer("наклон боковых зубов лингвально/небно, скученность в переднем участке - одночелюстной аппарат на обе челюсти", -1)
+		new Answer("Наклон боковых зубов лингвально/небно, скученность в переднем участке - одночелюстной аппарат на обе челюсти", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[26] = {
 	q: new Question("Результат", 26, false, true,
 	[
-		new Answer("Pетузия верхних резцов - конструкция аппарта с толкателем для передней группы зубов", -1)
+		new Answer("Pетузия верхних резцов - конструкция аппарта с толкателем для передней группы зубов", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[27] = {
 	q: new Question("Результат", 2, false, true,
 	[
-		new Answer("Oткрытый рот, нарушения функции - аппарат Френкеля, активатор Андрезена Гойпля", -1)
+		new Answer("Oткрытый рот, нарушения функции - аппарат Френкеля, активатор Андрезена Гойпля", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[28] = {
 	q: new Question("Результат", 28, false, true,
 	[
-		new Answer("незначительная скученность зубных рядов , носовое дыхание - эластопозиционер", -1)
+		new Answer("незначительная скученность зубных рядов , носовое дыхание - эластопозиционер", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[29] = {
-	q: new Question("Cужение верхнего и нижнего зубного ряда", 29, false, true,
+	q: new Question("Результат", 29, false, true,
 	[
-		new Answer("Pукообразный захватв съемном аппарате;частичная брекет-система", -1)
+		new Answer("Pукообразный захватв съемном аппарате;частичная брекет-система", -1),
+		new Answer("Следующий тест", 64)
 	])
 }
 questions[30] = {
 	q: new Question("Результат", 30, false, true,
 	[
-		new Answer("Губной бампер;частичная брекет-система", -1)
+		new Answer("Губной бампер;частичная брекет-система", -1),
+		new Answer("Следующий тест", 64)
 	])
-}/////////////////////////////////////////////////////////////////////
+}/////////////////////// ДЕРЕВО 3.1 ///////////////////////////////
 questions[31] = {
 	q: new Question("Кооперация", 31, true, false,
 	[
@@ -280,11 +299,12 @@ questions[35] = {
 questions[36] = {
 	q: new Question("Результат", 36, false, true,
 	[
-		new Answer("Рисунок ДЕРЕВО СЕМЬЯ ЧЕЛОВЕК", )
+		new Answer("Рисунок ДЕРЕВО СЕМЬЯ ЧЕЛОВЕК", -1),
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[37] = {
-	q: new Question("дать слепочную ложку для тренировки дома (даже в случае сканирования)", 37, false, false,
+	q: new Question("Дать слепочную ложку для тренировки дома (даже в случае сканирования)", 37, false, false,
 	[
 		new Answer("'Перевод' в группу контактен", 40),
 		new Answer("Консультация остеопата", 41),
@@ -306,14 +326,15 @@ questions[39] = {
 questions[40] = {
 	q: new Question("Результат", 40, false, true,
 	[
-		new Answer("'Перевод' в группу контактен", -1)
+		new Answer(`"Перевод" в группу контактен`, -1),
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[41] = {
-	q: new Question("", 41, false, true,
+	q: new Question("Результат", 41, false, true,
 	[
 		new Answer("Консультация остеопата", -1),
-		new Answer("", )
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[42] = {
@@ -326,13 +347,15 @@ questions[42] = {
 questions[43] = {
 	q: new Question("Результат", 43, false, true,
 	[
-		new Answer("возможно использование съемной аппаратуры", -1)
+		new Answer("возможно использование съемной аппаратуры", -1),
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[44] = {
 	q: new Question("Результат", 44, false, true,
 	[
-		new Answer("несъемная техника +курс профессионального ухода за полостью рта, решение о необходимости эндогенной профилактики", -1)
+		new Answer("несъемная техника +курс профессионального ухода за полостью рта, решение о необходимости эндогенной профилактики", -1),
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[45] = {
@@ -351,21 +374,24 @@ questions[46] = {
 questions[47] = {
 	q: new Question("Результат", 47, false, true,
 	[
-		new Answer("Изготовление съемного аппарата", -1)
+		new Answer("Изготовление съемного аппарата", -1),
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[48] = {
 	q: new Question("Результат", 48, false, true,
 	[
-		new Answer("Выбор несъемной конструкции с минимальным прилеганием к небу", -1)
+		new Answer("Выбор несъемной конструкции с минимальным прилеганием к небу", -1),
+		new Answer("Следующий тест", 0)
 	])
 }
 questions[49] = {
 	q: new Question("Результат", 49, false, true,
 	[
-		new Answer("Отсрочка ортодонтического лечения, работа с остеопатом", -1)
+		new Answer("Отсрочка ортодонтического лечения, работа с остеопатом", -1),
+		new Answer("Следующий тест", 0)
 	])
-}/////////////////////////////////////////////////////////////////////////////
+}////////////////////////// ДЕРЕВО 1 //////////////////////////////////////////
 questions[50] = {
 	q: new Question("Пациент с дистальной окклюзией", 50, true, false,
 	[
@@ -384,29 +410,33 @@ questions[51] = {
 questions[52] = {
 	q: new Question("Результат", 52, false, true,
 	[
-		new Answer("Данные диагностических моделей челюстей", -1)
+		new Answer("Данные диагностических моделей челюстей", -1),
+		new Answer("Следующий тест", 56)
 	])
 }
 questions[53] = {
 	q: new Question("Результат", 53, false, true,
 	[
-		new Answer("Данные телерентгенограммы", -1)
+		new Answer("Данные телерентгенограммы", -1),
+		new Answer("Следующий тест", 56)
 	])
 }
 questions[54] = {
 	q: new Question("Результат", 54, false, true,
 	[
-		new Answer("Данные ортопантомограммы/компьютерной томографии", -1)
+		new Answer("Данные ортопантомограммы/компьютерной томографии", -1),
+		new Answer("Следующий тест", 56)
 	])
 }
 questions[55] = {
 	q: new Question("Результат", 55, false, true,
 	[
-		new Answer("Кооперация/сотрудничество", -1)
+		new Answer("Кооперация/сотрудничество", -1),
+		new Answer("Следующий тест", 56)
 	])
-}
+}///////////////////// ДЕРЕВО 2 ///////////////////////////////////
 questions[56] = {
-	q: new Question("Клинический осмотр", 56, false, true,
+	q: new Question("Клинический осмотр", 56, true, false,
 	[
 		new Answer("Асимметрия лица", 57),
 		new Answer("Анамнез соматических заболеваний: заболевания ЛОР-органов/аллергический ринит/заболевания нервной системы/сколиоз", 58),
@@ -434,7 +464,8 @@ questions[59] = {
 questions[60] = {
 	q: new Question("Результат", 60, false, true,
 	[
-		new Answer("Трг в прямой проекции/КЛКТ/анализ лица в фас: дифференциальная диагностика скелетной и функциональной асимметрии", -1)
+		new Answer("Трг в прямой проекции/КЛКТ/анализ лица в фас: дифференциальная диагностика скелетной и функциональной асимметрии", -1),
+		new Answer("Следующий тест", 31)
 	])
 }
 questions[61] = {
@@ -446,15 +477,106 @@ questions[61] = {
 questions[62] = {
 	q: new Question("Результат", 62, false, true,
 	[
-		new Answer("самоконтроль/психолог/невролог", -1)
+		new Answer("самоконтроль/психолог/невролог", -1),
+		new Answer("Следующий тест", 31)
 	])
 }
 questions[63] = {
 	q: new Question("Результат", 63, false, true,
 	[
-		new Answer("Cопровождение у специалистов – ЛОР-врач, остеопат, ортопед, аллерголог", -1)
+		new Answer("Cопровождение у специалистов – ЛОР-врач, остеопат, ортопед, аллерголог", -1),
+		new Answer("Следующий тест", 31)
+	])
+}/////////////////////////// ДЕРЕВО 4 ////////////////////////////////////////////////////////
+questions[64] = {
+	q: new Question("Гииенический статус", 64, true, false,
+	[
+		new Answer("Перекус", 65),
+		new Answer("Уровень гигиены полости рта", 66)
 	])
 }
+questions[65] = {
+	q: new Question("Перекус", 65, false, false,
+	[
+		new Answer("До 4 раз", 67),
+		new Answer("4 раза и более", 68)
+	])
+}
+questions[66] = {
+	q: new Question("Уровень гигиены полости рта", 66, false, false,
+	[
+		new Answer("Хороший", 69),
+		new Answer("Удовлетворительный", 70),
+		new Answer("Неудовлетворительный и плохой", 71)
+	])
+}
+questions[67] = {
+	q: new Question("До 4 раз", 67, false, false,
+	[
+		new Answer("Контроль гигиены полости рта после приема пищи", 72)
+	])
+}
+questions[68] = {
+	q: new Question("4 раза и более", 68, false, false,
+	[
+		new Answer("Прием до витаминов и минералов, реминерализация, сокращение перекусов", 73)
+	])
+}
+questions[69] = {
+	q: new Question("Хороший", 69, false, false,
+	[
+		new Answer("Профессиональная гигиена полости рта, сопровождение  процесса ортодонтического лечения", 74)
+	])
+}
+questions[70] = {
+	q: new Question("Удовлетворительный", 70, false, false,
+	[
+		new Answer("Профессиональная гигиена полости рта, глубокое фторирование, домашние средства по уходу", 75)
+	])
+}
+questions[71] = {
+	q: new Question("Неудовлетворительный и плохой", 71, false, false,
+	[
+		new Answer("Уточнение анамнеза о поступлении минеральных компонентов с пищей прием препаратов фтора внутрь, применение методов реминерализации", 76)
+	])
+}
+questions[72] = {
+	q: new Question("Результат", 72, false, true,
+	[
+		new Answer("Контроль гигиены полости рта после приема пищи", -1),
+		new Answer("Итог", 999)
+	])
+}
+questions[73] = {
+	q: new Question("Результат", 73, false, true,
+	[
+		new Answer("Прием до витаминов и минералов, реминерализация, сокращение перекусов", -1),
+		new Answer("Итог", 999)
+	])
+}
+questions[74] = {
+	q: new Question("Результат", 74, false, true,
+	[
+		new Answer("Профессиональная гигиена полости рта, сопровождение  процесса ортодонтического лечения", -1),
+		new Answer("Итог", 999)
+	])
+}
+questions[75] = {
+	q: new Question("Результат", 75, false, true,
+	[
+		new Answer("Профессиональная гигиена полости рта, глубокое фторирование, домашние средства по уходу", -1),
+		new Answer("Итог", 999)
+	])
+}
+questions[76] = {
+	q: new Question("Результат", 76, false, true,
+	[
+		new Answer("Уточнение анамнеза о поступлении минеральных компонентов с пищей прием препаратов фтора внутрь, применение методов реминерализации", -1),
+		new Answer("Итог", 999)
+	])
+}
+
+var answers = []
 	
 //Сам тест
 quiz = new Quiz(questions, 0);
@@ -464,14 +586,16 @@ ChangeTree(1);
 
 function ChangeTree(value){
 	let btns = document.getElementsByClassName("buttonTree");
-	console.log(btns);
+	// console.log(btns);
 
-	// Делаем кнопки серыми
-	for(let i = 0; i < btns.length; i++)
-	{
-		btns[i].className = "buttonTree button_passive";
+	if (btns.length > 0){
+		// Делаем кнопки серыми
+		for(let i = 0; i < btns.length; i++)
+		{
+			btns[i].className = "buttonTree button_passive";
+		}
+		btns[value -1].className = "buttonTree button_correct";
 	}
-	btns[value -1].className = "buttonTree button_correct";
 	switch (value) {
 		case 1:{
 			quiz.current = 50;
@@ -496,6 +620,12 @@ function ChangeTree(value){
 
 			break;
 		}
+		case 5:{
+			quiz.current = 64;
+			Update();
+
+			break;
+		}
 		default:
 			break;
 	}
@@ -505,6 +635,9 @@ function ChangeTree(value){
 //Обновление теста
 function Update()
 {
+
+	
+
 	//Если есть, меняем вопрос в заголовке
 	headElem.innerHTML = quiz.questions[quiz.current].q.text;
 	// console.log(quiz.questions[0].q.answers.length);
@@ -525,23 +658,31 @@ function Update()
 
 		buttonsElem.appendChild(btn);
 	}
-	console.log(quiz.current);
-	let btn = document.createElement("button");
+	// console.log(quiz.current);
+	
+
+	if (quiz.questions[quiz.current].q.isEnd){
+		answers.push(quiz.current);
+		let btn = document.getElementById("-1");
+		btn.className = "button button_correct";
+		percents += 20;
+	} else {
+		let btn = document.createElement("button");
 		btn.className = "buttonSkip";
 
 		btn.innerHTML = "Пропустить";
 
 		btn.setAttribute("index", Number(quiz.current) + (1));
 		btn.setAttribute("id", Number(quiz.current) + (1));
-		console.log(Number(Number(quiz.current) + Number(1)));
+		// console.log(Number(Number(quiz.current) + Number(1)));
 		
 
 		buttonsElem.appendChild(btn);
-	
+	}
+
 	//Выводим номер текущего вопроса
 	pagesElem.innerHTML = "";
-	pagesElem.innerHTML = "№ " + (quiz.current);
-
+	pagesElem.innerHTML = percents + "%";
 	//Вызываем функцию, которая прикрепит события к новым кнопкам
 	Init();
 }
@@ -550,6 +691,7 @@ function Init()
 {
 	//Находим все кнопки
 	let btns = document.getElementsByClassName("button");
+	// console.log(btns);
 
 	for(let i = 0; i < btns.length; i++)
 	{
@@ -568,8 +710,8 @@ function Click(index)
 
 	//Находим все кнопки
 	let btns = document.getElementsByClassName("button");
-	console.log(quiz.questions[index].q.answers)
-	console.log(index);
+	// console.log(quiz.questions[index].q.answers)
+	// console.log(index);
 
 	// Делаем кнопки серыми
 	for(let i = 0; i < btns.length; i++)
@@ -579,9 +721,39 @@ function Click(index)
 	document.getElementById(index).className = "button button_correct";
 	// btns[index].className = "button button_correct";
 
-	quiz.current = index;
-	//Ждём секунду и обновляем тест
-	setTimeout(Update, 1000);
+	if (index == 999) {
+		///Выводим итог
+		Summary();
+	} else {
+		quiz.current = index;
+		//Ждём секунду и обновляем тест
+		setTimeout(Update, 1000);
+	}
+	
+	
+}
+
+function Summary()
+{
+	// console.log(answers);
+	str= "";
+	for (i = answers.length - 1; i >= 0 ; i--){
+		newDiv = document.createElement("div");
+		newContent = document.createTextNode(Number(i + 1) + "-й тест: " + questions[answers[i]].q.answers[0].text);
+		newDiv.appendChild(newContent);
+
+		headElem.insertAdjacentElement("afterend",newDiv);
+	}
+
+
+
+	//Если есть, меняем вопрос в заголовке
+	headElem.innerHTML = "Ответы:";
+	// console.log(quiz.questions[0].q.answers.length);
+
+	//Удаляем старые варианты ответов
+	buttonsElem.innerHTML = "";
+	pagesElem.innerHTML = percents + "%";
 }
 
 
